d19d58f3d5e95c24b05fbf7ea0d559e9ff9f91a9
tommydeejr <thomasdeejr@yahoo.com>
2025-07-07 01:36:14 +0000

    added Zoo tour

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.example.tourservice.TourManagementService;
 import com.example.tourservice.TravelAgentService;
 
 public class Application {
@@ -11,6 +12,10 @@ public class Application {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+        
+        TourManagementService tms = context.getBean(TourManagementService.class);
+        tms.createTour("Zoo tour", 100, true);
+        
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
 
